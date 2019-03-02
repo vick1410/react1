918ab77492c8cb60fe8077130c5da9b7883ae473
huascarm <huascarm@gmail.com>
2019-03-02 12:44:41 -0400

    subi codigo de la estructura general y el menu y pie

---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Menu extends Component {
+    render(){
+        return (
+            <div className="menu">
+                ESTA ES EL MENU
+            </div>
+        )
+    }
+}
+
+export default Menu;
---
+++ src/components/Pie.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Pie extends Component {
+    render(){
+        return (
+            <div className="foot">
+                ESTA ES EL PIE
+            </div>
+        )
+    }
+}
+
+export default Pie;
