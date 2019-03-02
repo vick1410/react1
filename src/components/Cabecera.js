cb654855bcb43ddc8ad1cfe3b795827debee1cd4
Virginia Mamani Cari <vicky.cari.1410@gmail.com>
2019-03-02 12:47:17 -0400

    estructura de la pagina, cabecera, cuerpo

---
+++ src/components/Cabecera.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Cabecera = props => {
+  return (
+    <header>
+      <h1>{props.titulo}</h1>
+    </header>
+  );
+};
+export default Cabecera;
---
+++ src/components/Cuerpo.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Cuerpo extends Component {
+  //state = {  }
+  render() {
+    return (
+      <div className="body">
+        <h1>ESTE ES EL CUERPO</h1>
+      </div>
+    );
+  }
+}
+
+export default Cuerpo;
